38540cc2339de5b00c34c2f258f299fb6bd779d3
Maria Tereza <mtereza.cl@gmail.com>
2024-01-22 16:23:56 -0300

    aula 6

--- cypress/integration/exercicio-e2e.spec.js
+++ cypress/integration/exercicio-e2e.spec.js
@@ -1,4 +1,27 @@
 /// <reference types="cypress" />
+let dadosLogin
+
+context('Funcionalidade Login', () => {
+    before(() => {
+        cy.fixture('perfil').then(perfil => {
+            dadosLogin = perfil
+        })
+    });
+
+    beforeEach(() => {
+        cy.visit('minha-conta')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Login com sucesso usando Comando customizado', () => {
+        cy.login(dadosLogin.usuario, dadosLogin.senha)
+        cy.get('.page-title').should('contain', 'Minha conta')
+    });
+})
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
